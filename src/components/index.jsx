79d6083faae632e54fd55f79de60920c9fafdfd9
Rafiul <rafiul13062001@gmail.com>
2023-04-22 01:44:44 +0600

    First React Project

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,5 +3,6 @@ import HeaderText from './headerText/HeaderText';
 import Details from './details/Details';
 import ProgramsThatCanBeTaken from './programsThatCanBeTaken/ProgramsThatCanBeTaken';
 import Testimonials from './testimonials/Testimonials';
+import UseMediaQuery from './usemediaquery/UseMediaQuery';
 
-export { NavigationBar, HeaderText, Details, ProgramsThatCanBeTaken, Testimonials }
+export { NavigationBar, HeaderText, Details, ProgramsThatCanBeTaken, Testimonials, UseMediaQuery }
--- src/components/navbar/NavigationBar.jsx
+++ src/components/navbar/NavigationBar.jsx
@@ -1,21 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import { Logo } from '../../assets/index'
+import { RiMenuFill, RiCloseFill } from 'react-icons/ri'
+import { UseMediaQuery } from '../index'
 
 const NavigationBar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false)
+    const isSize870 = UseMediaQuery('(min-width:870px)');
+
     return (
         <div className='container'>
             <nav>
                 <div className='logo-container'>
                     <img src={Logo} alt="img" />
                 </div>
-                <ul className='navigation-links'>
-                    <li className='navigation-link'>Home</li>
-                    <li className='navigation-link'>Program</li>
-                    <li className='navigation-link'>Blog</li>
-                    <li className='navigation-link'>About us</li>
-                    <li><button className='login-btn'>Log in</button></li>
-                </ul>
+                {
+                    isSize870 ?
+                        <ul className='navigation-links'>
+                            <li className='navigation-link'>Home</li>
+                            <li className='navigation-link'>Program</li>
+                            <li className='navigation-link'>Blog</li>
+                            <li className='navigation-link'>About us</li>
+                            <li><button className='login-btn'>Log in</button></li>
+                        </ul> :
+                        toggleMenu ?
+                            <div className='side-menu'>
+                                <ul className='navigation-links'>
+                                    <li className='navigation-link'>Home</li>
+                                    <li className='navigation-link'>Program</li>
+                                    <li className='navigation-link'>Blog</li>
+                                    <li className='navigation-link'>About us</li>
+                                    <li><button className='login-btn'>Log in</button></li>
+                                </ul>
+                            </div> : null
+                }
+                <div className='menuIcon'>
+                    {
+                        toggleMenu ?
+                        <RiCloseFill onClick={() => setToggleMenu(false)} size={40} />
+                        :
+                        <RiMenuFill onClick={() => setToggleMenu(true)} size={40} />
+                    }
+                </div>
             </nav>
         </div>
     )
